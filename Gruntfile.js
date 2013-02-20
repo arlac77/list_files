ccad9123ee62d55b3959080667d91e185944f822
Markus Felten <Markus.Felten@gmx.de>
2013-02-20 18:09:42 +0100

    started using grunt-vows

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,19 +3,27 @@ module.exports = function(grunt) {
 	pkg: grunt.file.readJSON('package.json'),
 	
     jslint: {
-      files: [
-        'list_files.js'
-      ]
+		files: [ 'list_files.js' ]
     },
 	jsdoc: {
 		dist : {
 			src: [ 'list_files.js' ], 
 			dest: 'doc'
 		}
-	}
+	},
+	vows : {
+	    options : {
+	      reporter : "spec"
+	    },
+	    all : {
+	      src : [ "<%= allTests %>" ]
+	    }
+	},
+	allTests : "tests/*"
   });
 
   grunt.registerTask('default', [ 'jslint' ] );
   grunt.loadNpmTasks('grunt-jsdoc');
   grunt.loadNpmTasks('grunt-jslint');
+  grunt.loadNpmTasks('grunt-vows-runner');
 };
