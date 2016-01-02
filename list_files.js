14134a807e606feca95cdfe921a426b36b31c40e
Markus Felten <Markus.Felten@gmx.de>
2016-01-02 18:27:46 +0100

    style: reformatting

--- list_files.js
+++ list_files.js
@@ -1,11 +1,11 @@
 "use strict";
 
-var path     = require('path')
-   ,fs       = require('fs')
-   ,async    = require('async');
+var path = require('path'),
+  fs = require('fs'),
+  async = require('async');
 
 /**
- * walks through a direcotry and calls iterator. 
+ * walks through a directory and calls iterator.
  * @param {String} base base directory
  * @param {String} dir  starting directory
  * @param {Regex} regex  file name pattern
@@ -13,38 +13,47 @@ var path     = require('path')
  * @param {Function(error)} callback  to be invoked at the end of the file list process
  *
  **/
-function list_files(base,dir,regex,iterator,callback)
-{
-    fs.readdir(path.join(base,dir), function(error, files) {
-        if(error) { iterator(error); if(callback) callback(error); return; }
+function list_files(base, dir, regex, iterator, callback) {
+  fs.readdir(path.join(base, dir), function (error, files) {
+    if (error) {
+      iterator(error);
+      if (callback) callback(error);
+      return;
+    }
 
-		var f = [];
+    var f = [];
 
-		for(var i in files) {
-			f.push(path.join(base,dir,files[i]));
-		}
+    for (var i in files) {
+      f.push(path.join(base, dir, files[i]));
+    }
 
-		var subdirs = 0;
+    var subdirs = 0;
 
-		async.map(f,fs.stat,function(error, stats) {
-        	if(error) { iterator(error); if(callback) callback(error); return; }
-			for(var i in stats) {
-				var file = files[i];
-	            if(file.charAt(0) === '.') continue;
-	
-	        	if(stats[i].isDirectory()) {
-					subdirs++;
-	      			list_files(base,path.join(dir,file),regex,iterator,function(error) { subdirs--; if(callback && subdirs === 0) callback(error); });
-				}
-	       		else {
-					if(file.match(regex))
-	       				iterator(undefined,path.join(dir,file),stats[i]);
-				}
-			}
+    async.map(f, fs.stat, function (error, stats) {
+      if (error) {
+        iterator(error);
+        if (callback) callback(error);
+        return;
+      }
+      for (var i in stats) {
+        var file = files[i];
+        if (file.charAt(0) === '.') continue;
 
-			if(callback && subdirs === 0) callback(error);
-		});
+        if (stats[i].isDirectory()) {
+          subdirs++;
+          list_files(base, path.join(dir, file), regex, iterator, function (error) {
+            subdirs--;
+            if (callback && subdirs === 0) callback(error);
+          });
+        } else {
+          if (file.match(regex))
+            iterator(undefined, path.join(dir, file), stats[i]);
+        }
+      }
+
+      if (callback && subdirs === 0) callback(error);
     });
+  });
 }
 
-exports.list_files        = list_files;
+exports.list_files = list_files;
